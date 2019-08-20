436592f400e2381fa9fa37099a772b507bf76c68
rfortiz <raphael.ortiz@fmi.ch>
2019-08-20 18:45:04 +0200

    changed from mean to max projection

--- improc/segmentation.py
+++ improc/segmentation.py
@@ -13,7 +13,7 @@ def segment_from_projections(image, spacing=1, sigma=2):
     for proj_axis, (d1, d2) in enumerate(
             reversed(list(itertools.combinations(range(image.ndim), 2)))):
 
-        proj = image.mean(axis=proj_axis)
+        proj = image.max(axis=proj_axis)
         smoothed_proj = gaussian_filter(proj, sigma=sigma / spacing[[d1, d2]])
         mask_proj = smoothed_proj > threshold_otsu(smoothed_proj)
         mask = np.logical_and(mask, np.expand_dims(mask_proj, axis=proj_axis))
