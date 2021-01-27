4d463ae1d6882c77e87ef18f0729755e7b799b9e
rfortiz <raphael.ortiz@fmi.ch>
2021-01-27 14:01:16 +0100

    fix comparison between numpy and python object

--- improc/resample.py
+++ improc/resample.py
@@ -26,27 +26,35 @@ def resample_labels(labels, factor):
         mask = labels[loc] == l
 
         # get label coordinates and upsample
-        coords = np.round(np.where(mask) * factor[:, None]).astype(int)
-        coords = tuple([*coords])
+        #         coords = np.round(np.where(mask) * factor[:, None]).astype(int)
+        #         coords = tuple([*coords])
+
+        # TODO fix
+        resampled_mask = rescale(mask.astype(np.float32),
+                                 scale=factor,
+                                 multichannel=False,
+                                 anti_aliasing=False,
+                                 preserve_range=True,
+                                 order=1)
 
         # create upsampled mask
-        resampled_mask = np.zeros(np.round(mask.shape * factor).astype(int),
-                                  dtype=np.float32)
-        resampled_mask[coords] = np.prod(factor)
+        #         resampled_mask = np.zeros(np.round(mask.shape * factor).astype(int),
+        #                                   dtype=np.float32)
+        #         resampled_mask[coords] = np.prod(factor)
 
-        # interpolate upsampled image
-        resampled_mask = gaussian_filter(resampled_mask, sigma=factor.max())
+        #         # interpolate upsampled image
+        #         resampled_mask = gaussian_filter(resampled_mask, sigma=factor.max())
 
         loc_resampled = tuple(
             slice(
-                np.round(f * s.start).astype(int),
-                np.round(f * s.start).astype(int) + s_size)
+                np.floor(f * s.start).astype(int),
+                np.floor(f * s.start).astype(int) + s_size)
             for s, f, s_size in zip(loc, factor, resampled_mask.shape))
 
         # TODO figure out general solution/threshold for different factors
         # 0.48 instead of 0.5 --> slightly larger label mask to prevent introduction of background between touching labels
         # not general, e.g. works for (2,0.26,26) to 1, but not from 25 to 37x
-        resampled_labels[loc_resampled][resampled_mask > 0.48] = l
+        resampled_labels[loc_resampled][resampled_mask > 0.5] = l
 
     return resampled_labels
 
@@ -66,27 +74,29 @@ def match_spacing(img,
     input_dtype = img.dtype
     src_spacing = np.asarray(src_spacing)
 
-    if dst_spacing == 'isotropic':
+    if isinstance(dst_spacing, str) and dst_spacing == 'isotropic':
         scale = src_spacing / src_spacing.min()
     else:
         dst_spacing = np.asarray(dst_spacing)
         scale = np.array(src_spacing) / np.array(dst_spacing)
 
     if image_type == 'greyscale':
-        return rescale(img.astype(np.float32),
+        return rescale(img,
                        scale=scale,
                        multichannel=False,
                        anti_aliasing=True,
+                       preserve_range=True,
                        order=1).astype(input_dtype)
 
     elif image_type == 'label_interp':
         return resample_labels(img, scale)
 
     elif image_type == 'label_nearest':
-        return rescale(img.astype(np.float32),
+        return rescale(img,
                        scale=scale,
                        multichannel=False,
                        anti_aliasing=False,
+                       preserve_range=True,
                        order=0).astype(input_dtype)
 
     else:
